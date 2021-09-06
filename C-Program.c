a97674afd6e5cd4462dff98594291cd93685c179
Shriyanshi Srivastava <54525894+ShriyanshiSrivastava@users.noreply.github.com>
2021-09-06 15:55:04 +0530

    This is my C program , an example on how to upload it to github repository.

---
+++ C-Program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int number1 , number2, sum;
+
+    printf("Enter Number 1:- ");
+    scanf("%d", &number1);
+    printf("Enter Number 2:- ");
+    scanf("%d",&number2);
+    sum= number1+number2;
+    printf("Sum is %d", sum);
+    return 0;
+
+}
